585a1bf4d2a43b9b8d36abe10445d0a0f3e2fda9
Juan Pablo <totomarion@gmail.com>
2022-11-29 20:54:30 -0300

    Instalar ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    }),
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 export class AppModule { }
