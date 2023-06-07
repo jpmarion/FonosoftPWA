64422ac31985ce7f53e022187627bd12df5da95e
Juan Pablo <totomarion@gmail.com>
2023-06-07 20:27:28 -0300

    Craer menu component

--- src/app/pages/home/menu/menu.component.ts
+++ src/app/pages/home/menu/menu.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { SiNoDialogComponent } from '../../dialog/si-no-dialog/si-no-dialog.component';
+import { ConfigDialogComponent } from '../config-dialog/config-dialog.component';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  constructor(
+    private dialog: MatDialog,
+    private router: Router,
+    private dialogoSalir: MatDialog
+  ) { }
 
+  irPacientes(): void {
+    this.router.navigate(['home', { outlets: { homecomponent: ['paciente'] } }]);
+  }
+
+  irMedicos(): void {
+    this.router.navigate(['home', { outlets: { homecomponent: ['medico'] } }]);
+  }
+
+  abrirConfigDialog(): void {
+    const dialogRef = this.dialog.open(ConfigDialogComponent);
+  }
+
+  logOut(): void {
+    this.dialogoSalir.open(SiNoDialogComponent, {
+      data: { titulo: "NAV.tituloSalirDialog", mensaje: "NAV.msjSalirDialog" }
+    })
+      .afterClosed()
+      .subscribe((si: boolean) => {
+        if (si) {
+          localStorage.removeItem('idUsuario');
+          localStorage.removeItem('token');
+          this.router.navigate(['/']);
+        }
+      })
+  }
 }
